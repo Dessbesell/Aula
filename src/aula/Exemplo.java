58a37707304db672bddd87dceeb0031597582837
Diana <Diana@DESKTOP-K455FPD>
2022-11-09 22:18:12 -0300

    Incluído o método SayHelloManyTimes

--- src/aula/Exemplo.java
+++ src/aula/Exemplo.java
@@ -1,11 +1,17 @@
 package aula;
 
 public class Exemplo {
+	public static void SayHelloManyTimes(int times) {
+		for (int i=0; i < times; i++) {
+			SayHello();
+		}
+	}
+	
 	public static void SayHello() {
 		System.out.println("Olá, mundo!");
 	}
 	
 	public static void main(String[] args) {
-		SayHello();	
+		SayHelloManyTimes(5);	
 	}
 }
